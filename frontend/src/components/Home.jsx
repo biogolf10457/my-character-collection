0fe46af98cbaed649efa94f2b78cf8075072528c
biogolf10457 <biogolf10457@gmail.com>
2025-02-20 15:00:51 +0700

    update homepage, navbar
    - add mascot
    - add icon

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import mascot from "../assets/mcc.png";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="min-h-[100vh] px-4 py-6 sm:px-8 lg:px-12 lg:py-10 bg-amber-200">
-      <div className="flex flex-col lg:flex-row items-center justify-around">
-        <div className="w-[75%] lg:w-[40%] bg-amber-900">Image</div>
-        <div className="w-[75%] lg:w-[40%] bg-amber-900">
-          <h1 className="font-chewy text-2xl lg:text-3xl text-center">
-            Welcome to{" "}
-            <span className="text-nowrap">My Character Collection</span>
+    <div className="min-h-[100vh] px-4 py-6 sm:px-8 lg:px-12 lg:py-10 bg-linear-to-b from-amber-100 to-amber-200">
+      <div className="flex flex-col lg:flex-row items-center justify-around space-y-10 lg:space-y-0">
+        <div className="w-[55%] lg:w-[35%]">
+          <img src={mascot} className="w-full object-contain" />
+        </div>
+        <div className="w-[75%] lg:w-[40%]">
+          <h2 className="font-chewy  text-2xl lg:text-4xl">Welcome to</h2>
+          <h1 className="mt-2 font-chewy text-3xl lg:text-5xl text-amber-500 uppercase">
+            My Character Collection
           </h1>
-          <p className="mt-4 font-roboto text-center">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque,
-            id. Hic minima delectus et natus voluptas culpa, fugit sunt
-            voluptates sapiente molestias dolores ad temporibus possimus qui,
-            architecto, exercitationem eius!
+          <p className="mt-6 font-roboto text-sm lg:text-lg">
+            Did you forget your original characters information? Let's try My
+            Character Collection! Inform your original characters information
+            and collect in your profile. Easy to see and organize your
+            characters.
           </p>
+          <Link to="/register">
+            <button className="button mt-6">GET STARTED</button>
+          </Link>
         </div>
       </div>
     </div>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import mascotIcon from "../assets/mccIcon.png";
 
 const Navbar = () => {
   return (
-    <div className="max-w-full px-2 py-4 sm:px-6 lg:px-8 bg-gray-700">
+    <div className="max-w-full px-2 py-4 sm:px-6 lg:px-8 bg-amber-500">
       <div className="flex items-center justify-between">
         <div>
-          <Link to="/">Icon</Link>
+          <Link to="/">
+            <div className="h-10 flex items-center space-x-3">
+              <img src={mascotIcon} className="h-full" />
+              <div className="font-chewy text-xl text-amber-50 uppercase">
+                My Character Collection
+              </div>
+            </div>
+          </Link>
         </div>
         <div className="flex space-x-10">
           <Link className="font-chewy text-lg" to="/login">
