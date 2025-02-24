42721cc3ba64df92e112a223fce6d60f327dac15
biogolf10457 <biogolf10457@gmail.com>
2025-02-24 16:05:13 +0700

    update image handle

--- frontend/src/components/CharacterCard.jsx
+++ frontend/src/components/CharacterCard.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import noImage from "../assets/noImage.jpg";
 
-const CharacterCard = ({ name }) => {
+const CharacterCard = ({ name, image }) => {
   return (
     <div className="relative px-4 py-3 bg-orange-100 rounded-xl shadow-md hover:scale-105 hover:-rotate-1 transition-all">
-      <div className="inline-block w-1/3 aspect-square bg-amber-300 rounded-full inset-shadow-sm align-middle"></div>
+      <div className="inline-block w-1/3 aspect-square bg-white rounded-full border-amber-900 border-4 overflow-hidden align-middle">
+        <img
+          className="w-full h-full object-contain"
+          src={image === "" ? noImage : image}
+          alt="character image"
+        />
+      </div>
       <div className="inline-block w-2/3 pl-4 text-2xl font-chewy align-middle">
         {name}
       </div>
