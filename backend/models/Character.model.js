c8894ed4f778cdc3e301e8bd7ab591cf617c32ae
biogolf10457 <biogolf10457@gmail.com>
2025-02-24 16:04:35 +0700

    change required for character model

--- backend/models/Character.model.js
+++ backend/models/Character.model.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 const characterSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    gender: { type: String, required: true },
-    age: { type: String, required: true },
-    height: { type: String, required: true },
-    weight: { type: String, required: true },
-    image: { type: String, required: true },
-    information: { type: String, required: true },
+    gender: { type: String, required: false },
+    age: { type: String, required: false },
+    height: { type: String, required: false },
+    weight: { type: String, required: false },
+    image: { type: String, required: false },
+    information: { type: String, required: false },
     ownerid: { type: String, required: true },
   },
   { timestamps: true }
