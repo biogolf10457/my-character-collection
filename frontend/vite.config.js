bf7468e212d4314008d3d6e0b2946213009a4343
biogolf10457 <biogolf10457@gmail.com>
2025-03-04 19:52:35 +0700

    update proxy server fix cors issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,30 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      // Target is your backend API
+      "/api": {
+        target: "https://my-character-collection-server.vercel.app",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+
+        configure: (proxy, options) => {
+          proxy.on("error", (err, _req, _res) => {
+            console.log("error", err);
+          });
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Request sent to target:", req.method, req.url);
+          });
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Response received from target:",
+              proxyRes.statusCode,
+              req.url
+            );
+          });
+        },
+      },
+    },
+  },
 });
